20a4c24660e223279c3f9c8a341b553251263e1e
Fule <Fule@fule.dk>
2021-03-27 20:34:17 +0100

    might break update

--- Win10_20H2/Scripts/Cleanup.ps1
+++ Win10_20H2/Scripts/Cleanup.ps1
@@ -16,14 +16,10 @@ $env:PATH = ($env:PATH -split ';' | Where-Object { $_ -notlike '*Boxstarter*' })
 $env:PSModulePath = ($env:PSModulePath -split ';' | Where-Object { $_ -notlike '*Boxstarter*' }) -join ';'
 [Environment]::SetEnvironmentVariable('PSModulePath', $env:PSModulePath, 'Machine')
 
-#Remove-Item -Path "C:\ProgramData\Boxstarter" -Recurse -Force
-#Remove-Item -Path "C:\Users\Public\Desktop\Boxstarter Shell.lnk" -Force
-#Remove-Item -Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Boxstarter" -Recurse -Force
-
 Uninstall-Module WindowsBox.AutoLogon -Force
 Uninstall-Module WindowsBox.Compact -Force
 Uninstall-Module WindowsBox.Hibernation -Force
 Uninstall-Module WindowsBox.Network -Force
 
 #Makes sure WinRM is in a secure state and ready for reconfiguration
-choco install undo-winrmconfig-during-shutdown -y
+#choco install undo-winrmconfig-during-shutdown -y
