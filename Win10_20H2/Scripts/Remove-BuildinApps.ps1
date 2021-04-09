a887397df007d8cd9dbb4f2b9b89379d3549a09e
Rasmus Brandt <rasmusbrandt92@gmail.com>
2021-04-09 22:35:20 +0200

    minor update

--- Win10_20H2/Scripts/Remove-BuildinApps.ps1
+++ Win10_20H2/Scripts/Remove-BuildinApps.ps1
@@ -76,29 +76,6 @@ Begin {
     ))
 }
 Process {
-    # Functions
-    function Write-Output {
-        param(
-            [parameter(Mandatory=$true, HelpMessage="Value added to the RemovedApps.log file.")]
-            [ValidateNotNullOrEmpty()]
-            [string]$Value,
-
-            [parameter(Mandatory=$false, HelpMessage="Name of the log file that the entry will written to.")]
-            [ValidateNotNullOrEmpty()]
-            [string]$FileName = "RemovedApps.log"
-        )
-        # Determine log file location
-        $LogFilePath = Join-Path -Path $env:windir -ChildPath "Temp\$($FileName)"
-
-        # Add value to log file
-        try {
-            Out-File -InputObject $Value -Append -NoClobber -Encoding Default -FilePath $LogFilePath -ErrorAction Stop
-        }
-        catch [System.Exception] {
-            Write-Warning -Message "Unable to append log entry to $($FileName) file"
-        }
-    }
-
     # Initial logging
     Write-Output -InputObject "Starting built-in AppxPackage, AppxProvisioningPackage and Feature on Demand V2 removal process"
 
